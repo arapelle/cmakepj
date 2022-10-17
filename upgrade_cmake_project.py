3aa982ea8836220d4887a8939f293c6ac2e14c41
Aymeric Pellé <aympelle@gmail.com>
2022-10-17 18:55:00 +0200

    v0.2: Refactoring: Add class CMakeListsFile, ProjectCMakeListsFile.

--- upgrade_cmake_project.py
+++ upgrade_cmake_project.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import glob
 import os
+import pathlib
 import re
+from copy import deepcopy
 from enum import Enum, IntEnum
 
 import packaging.version
@@ -13,6 +15,86 @@ class ReleaseComponent(IntEnum):
     MAJOR = 0,
     MINOR = 1,
     PATCH = 2
+
+
+class CMakeListsFile:
+    def __init__(self, contents=""):
+        self.__file_path = None
+        self._contents = contents
+
+    @property
+    def file_path(self):
+        return self.file_path
+
+    @property
+    def contents(self):
+        return self._contents
+
+    def load(self, file_path):
+        if not pathlib.Path(file_path).exists():
+            raise ValueError(file_path)
+        self.__file_path = file_path
+        with open(self.__file_path, "r", newline='\n') as file:
+            self._contents = file.read()
+
+    def save(self, file_path=None):
+        if file_path is None:
+            file_path = self.__file_path
+        else:
+            self.__file_path = file_path
+        with open(file_path, "w", newline='\n') as file:
+            file.write(self._contents)
+
+
+class ProjectCMakeListsFile(CMakeListsFile):
+    def __init__(self, file_path):
+        super(ProjectCMakeListsFile, self).__init__()
+        self.load(file_path)
+        self.__find_project_version()
+
+    @property
+    def project_version(self):
+        return self.__project_version
+
+    @property
+    def project_version_declaration(self):
+        return self.__project_version_declaration
+
+    def __find_project_version(self):
+        pr_version_regex = ProjectCMakeListsFile.__project_version_regex()
+        pr_version_match: re.Match = pr_version_regex.search(self._contents)
+        if pr_version_match is None:
+            raise Exception("ERROR - Project version not found.")
+        self.__project_version_declaration = pr_version_match.group(0)
+        self.__project_version = pr_version_match.group(1)
+
+    def upgraded_version(self, release_comp: ReleaseComponent):
+        ver = Version(self.__project_version)
+        epoch = ver.epoch
+        v_release = list(ver.release)
+        v_release[release_comp] += 1
+        release_segment = ".".join(str(x) for x in v_release)
+        pre = "".join(str(x) for x in ver.pre) if ver.is_prerelease else ''
+        post = f".post{ver.post}" if ver.is_postrelease else ''
+        dev = f".dev{ver.dev}" if ver.is_devrelease else ''
+        local = f"+{ver.local}" if ver.local else ''
+        upgrade = f"{epoch}!{release_segment}{pre}{post}{dev}{local}"
+        new_version = str(Version(upgrade))
+        return new_version
+
+    def upgraded_file(self, release_comp: ReleaseComponent):
+        new_file: ProjectCMakeListsFile = deepcopy(self)
+        new_file.__project_version = self.upgraded_version(release_comp)
+        new_file.__project_version_declaration = f"VERSION {new_file.__project_version} #project-version"
+        new_file._contents = new_file._contents.replace(self.__project_version_declaration,
+                                                        new_file.__project_version_declaration)
+        return new_file
+
+    @staticmethod
+    def __project_version_regex():
+        pr_version_regex = re.compile(r"VERSION\s+(" + packaging.version.VERSION_PATTERN + r")\s*\#project-version",
+                                      re.VERBOSE | re.IGNORECASE)
+        return pr_version_regex
 
 
 class CMakeProjectUpgrader:
@@ -96,5 +178,9 @@ upgrader = CMakeProjectUpgrader(".")
 project_version, new_project_version = upgrader.upgrade_version_in_project_cmakelists_txt(ReleaseComponent.MINOR)
 upgrader.upgrade_version_in_examples_cmakelists_txt(project_version, new_project_version)
 # upgrader.commit_start_version(new_project_version)
+
+cmake_file = ProjectCMakeListsFile('CMakeLists.txt')
+new_cmake_file = cmake_file.upgraded_file(ReleaseComponent.MINOR)
+new_cmake_file.save()
 
 print('EXIT SUCCESS')
