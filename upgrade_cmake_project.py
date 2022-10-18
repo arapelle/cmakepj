a339cf4db54f238b5e999086437182421be40f77
Aymeric Pellé <aympelle@gmail.com>
2022-10-18 16:52:42 +0200

    v0.2: Refactored: Same features but with CMakeProject and CMakeListsFile classes.

--- upgrade_cmake_project.py
+++ upgrade_cmake_project.py
@@ -45,28 +45,52 @@ class CMakeListsFile:
         with open(file_path, "w", newline='\n') as file:
             file.write(self._contents)
 
+    def update_dependency_version(self, current_version, new_version):
+        fp_version_regex = CMakeListsFile.__find_package_version_regex(current_version)
+        fp_version_match: re.Match = fp_version_regex.search(self._contents)
+        if fp_version_match is not None:
+            print(f"    matching. converting to new_version: {new_version}")
+#           contents.replace()
+
+    @staticmethod
+    def __find_package_version_regex(current_version):
+        print(f"current_version: {current_version}")
+        version_regex = re.compile(r"find_package\(([^\s]+\s+)" + current_version + r"(\s+.*\#project-version)",
+                                      re.VERBOSE | re.IGNORECASE)
+        return version_regex
+
 
 class ProjectCMakeListsFile(CMakeListsFile):
     def __init__(self, file_path):
         super(ProjectCMakeListsFile, self).__init__()
+        self.__project_version = None
         self.load(file_path)
-        self.__find_project_version()
+
+    def load(self, file_path):
+        super().load(file_path)
+        # self.__project_name = ...
+        # self.__package_name = ...
+        # self.__project_full_version = ...
+        self.__project_version = self.__find_project_version()
 
     @property
     def project_version(self):
         return self.__project_version
 
-    @property
-    def project_version_declaration(self):
-        return self.__project_version_declaration
-
     def __find_project_version(self):
+        pr_version_match = self.__find_project_version_match()
+        return pr_version_match.group(1)
+
+    def __find_project_version_declaration(self):
+        pr_version_match = self.__find_project_version_match()
+        return pr_version_match.group(0)
+
+    def __find_project_version_match(self):
         pr_version_regex = ProjectCMakeListsFile.__project_version_regex()
         pr_version_match: re.Match = pr_version_regex.search(self._contents)
         if pr_version_match is None:
             raise Exception("ERROR - Project version not found.")
-        self.__project_version_declaration = pr_version_match.group(0)
-        self.__project_version = pr_version_match.group(1)
+        return pr_version_match
 
     def upgraded_version(self, release_comp: ReleaseComponent):
         ver = Version(self.__project_version)
@@ -82,13 +106,14 @@ class ProjectCMakeListsFile(CMakeListsFile):
         new_version = str(Version(upgrade))
         return new_version
 
-    def upgraded_file(self, release_comp: ReleaseComponent):
-        new_file: ProjectCMakeListsFile = deepcopy(self)
-        new_file.__project_version = self.upgraded_version(release_comp)
-        new_file.__project_version_declaration = f"VERSION {new_file.__project_version} #project-version"
-        new_file._contents = new_file._contents.replace(self.__project_version_declaration,
-                                                        new_file.__project_version_declaration)
-        return new_file
+    def upgrade_project_version(self, release_comp: ReleaseComponent):
+        self.__project_version = self.upgraded_version(release_comp)
+        new_pj_version_declaration = self.__format_project_version_declaration(self.__project_version)
+        self._contents = self._contents.replace(self.__find_project_version_declaration(), new_pj_version_declaration)
+
+    @staticmethod
+    def __format_project_version_declaration(project_version):
+        return f"VERSION {project_version} #project-version"
 
     @staticmethod
     def __project_version_regex():
@@ -103,86 +128,16 @@ class CMakeProject:
         self.__project_cmakelists_file = ProjectCMakeListsFile(f"{repository_path}/CMakeLists.txt")
 
     def upgrade(self, release_comp: ReleaseComponent):
-        new_cmake_file = self.__project_cmakelists_file.upgraded_file(release_comp)
-        self.__project_cmakelists_file = new_cmake_file
-        new_cmake_file.save()
-
-    def checkout_develop_branch(self, branch):
-        print(f"INFO - Checkout {branch}")
-        self.__repository.git.checkout(f'{branch}')
-
-    def commit_start_version(self, new_project_version):
-        print("INFO - Add CMakeLists.txt to index")
-        self.__repository.index.add(["CMakeLists.txt"])
-        print("INFO - Commit")
-        self.__repository.index.commit(f"v{new_project_version}: Start version {new_project_version}.")
-
-
-
-class CMakeProjectUpgrader:
-    def __init__(self, repository_path):
-        self.__repository = Repo(repository_path)
-
-    def upgraded_version(self, ver_str: str, release_comp: ReleaseComponent):
-        ver = Version(ver_str)
-        epoch = ver.epoch
-        v_release = list(ver.release)
-        v_release[release_comp] += 1
-        release_segment = ".".join(str(x) for x in v_release)
-        pre = "".join(str(x) for x in ver.pre) if ver.is_prerelease else ''
-        post = f".post{ver.post}" if ver.is_postrelease else ''
-        dev = f".dev{ver.dev}" if ver.is_devrelease else ''
-        local = f"+{ver.local}" if ver.local else ''
-        upgrade = f"{epoch}!{release_segment}{pre}{post}{dev}{local}"
-        new_ver = str(Version(upgrade))
-        return new_ver
-
-    def upgrade_version_in_project_cmakelists_txt(self, release_comp: ReleaseComponent):
-        print("INFO - Upgrade project version")
-        # Version Identification and Dependency Specification: https://peps.python.org/pep-0440/
-        # contents = "VERSION 2022!1.2.3.4rc0.dev44+nike.1.2 #project-version"
-        with open("CMakeLists.txt", "r", newline='\n') as file:
-            contents = file.read()
-        pr_version_regex = self.__project_version_regex()
-        pr_version_match: re.Match = pr_version_regex.search(contents)
-        if pr_version_match is None:
-            raise Exception("ERROR - Project version not found.")
-        matching_str = pr_version_match.group(0)
-        project_version = pr_version_match.group(1)
-        print(f"matching_str: {matching_str}")
-        print(f"project_version: {project_version}")
-        new_project_version = self.upgraded_version(project_version, release_comp)
-        new_str = f"VERSION {new_project_version} #project-version"
-        contents = contents.replace(matching_str, new_str)
-        # with open("CMakeLists.txt", "w", newline='\n') as file:
-        #     file.write(contents)
-        return project_version, new_project_version
-
-    def __project_version_regex(self):
-        pr_version_regex = re.compile(r"VERSION\s+(" + packaging.version.VERSION_PATTERN + r")\s*\#project-version",
-                                      re.VERBOSE | re.IGNORECASE)
-        return pr_version_regex
-
-    def upgrade_version_in_examples_cmakelists_txt(self, current_version, new_version):
-        print("INFO - Upgrade other project files")
-        fp_version_regex = self.__find_package_version_regex(current_version)
+        old_project_version = self.__project_cmakelists_file.project_version
+        self.__project_cmakelists_file.upgrade_project_version(release_comp)
+        new_project_version = self.__project_cmakelists_file.project_version
+        self.__project_cmakelists_file.save()
         cmakelists_files = glob.glob("**/CMakeLists.txt", recursive=True)
-        for cmakelists_file in cmakelists_files:
-            print(f"  Treating file '{cmakelists_file}'")
-            with open(cmakelists_file, "r", newline='\n') as file:
-                contents = file.read()
-                fp_version_match: re.Match = fp_version_regex.search(contents)
-                if fp_version_match is None:
-                    continue
-                print("    matching")
-#                contents.replace()
-        pass
-
-    def __find_package_version_regex(self, current_version):
-        print(f"current_version: {current_version}")
-        version_regex = re.compile(r"find_package\(([^\s]+\s+)" + current_version + r"(\s+.*\#project-version)",
-                                      re.VERBOSE | re.IGNORECASE)
-        return version_regex
+        for cmakelists_file_path in cmakelists_files:
+            print(f"  Treating file '{cmakelists_file_path}'")
+            cmakelists_file = CMakeListsFile()
+            cmakelists_file.load(cmakelists_file_path)
+            cmakelists_file.update_dependency_version(old_project_version, new_project_version)
 
     def checkout_develop_branch(self, branch):
         print(f"INFO - Checkout {branch}")
@@ -194,12 +149,6 @@ class CMakeProjectUpgrader:
         print("INFO - Commit")
         self.__repository.index.commit(f"v{new_project_version}: Start version {new_project_version}.")
 
-
-upgrader = CMakeProjectUpgrader(".")
-# upgrader.checkout_develop_branch("develop")
-project_version, new_project_version = upgrader.upgrade_version_in_project_cmakelists_txt(ReleaseComponent.MINOR)
-upgrader.upgrade_version_in_examples_cmakelists_txt(project_version, new_project_version)
-# upgrader.commit_start_version(new_project_version)
 
 cmake_project = CMakeProject(".")
 cmake_project.upgrade(ReleaseComponent.MINOR)
